8b688b4abb7c0259295a25935edaeb28add0e626
Popcorn245 <Bobby@madnesslabs.net>
2018-10-13 11:59:17 -0500

    Create text to speech component

---
+++ src/components/migrant-text-to-speech/migrant-text-to-speech.tsx
@@ -0,0 +1,15 @@
+import { Component } from '@stencil/core';
+
+@Component({
+  tag: 'migrant-text-to-speech',
+  styleUrl: 'migrant-text-to-speech.css'
+})
+export class MigrantTextToSpeech {
+  render() {
+    return (
+      <div>
+        <p>Hello MigrantTextToSpeech!</p>
+      </div>
+    );
+  }
+}
