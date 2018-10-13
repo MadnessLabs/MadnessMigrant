7e87958e44942e5fb838d7d2ee633e1747e18730
Popcorn245 <Bobby@madnesslabs.net>
2018-10-13 09:08:58 -0500

    Add netlify support

---
+++ enjin.netlify.ts
@@ -0,0 +1,14 @@
+export default {
+  app: {
+    url: 'https://madnessmigrant.netlify.com',
+    apiUrl: 'https://us-central1-madnessmigrant.cloudfunctions.net'
+  },
+  firebase: {
+    apiKey: "AIzaSyCp3gb7o9ovols8lVDBQq2IgADZPpLxzbc",
+    authDomain: "madnessmigrant.firebaseapp.com",
+    databaseURL: "https://madnessmigrant.firebaseio.com",
+    projectId: "madnessmigrant",
+    storageBucket: "madnessmigrant.appspot.com",
+    messagingSenderId: "540849659656"
+  }
+};
