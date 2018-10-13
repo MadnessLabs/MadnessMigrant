1ff3ff8495908eb4cb1898d0827a65155369bc9a
aaronmhappe <aaron01happe@gmail.com>
2018-10-12 21:34:16 -0500

    set up environment file

---
+++ src/global/environment.ts
@@ -0,0 +1,17 @@
+export default {
+  app: {
+    url: "http://localhost:3333",
+    apiUrl: "http://localhost:5000/disaster-bot-test/us-central1"
+  },
+  stripe: {
+    key: "pk_test_HaVQgiY2OEUENQJhaDkuoOTV"
+  },
+  firebase: {
+    apiKey: "AIzaSyCB3YeJM-KzZ-oIawgwuQ9S6-P-aeDjt5M",
+    authDomain: "migrant-4890d.firebaseapp.com",
+    databaseURL: "https://migrant-4890d.firebaseio.com",
+    projectId: "migrant-4890d",
+    storageBucket: "migrant-4890d.appspot.com",
+    messagingSenderId: "649034695380"
+  }
+};
---
+++ src/services/config.ts
@@ -0,0 +1,10 @@
+/**
+ * @module Services
+ */
+import config from '../global/environment';
+
+export class ConfigService {
+  public get(name: string) {
+    return config[name];
+  }
+}
