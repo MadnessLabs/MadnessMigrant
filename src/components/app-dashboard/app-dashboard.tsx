d524d010ad41b31378e3978c003ba4665b6c6e74
aaronmhappe <aaron01happe@gmail.com>
2018-10-13 22:15:17 -0500

    style c hanges

--- src/components/app-dashboard/app-dashboard.tsx
+++ src/components/app-dashboard/app-dashboard.tsx
@@ -2,7 +2,7 @@ import { Component, State } from '@stencil/core';
 
 @Component({
     tag: 'app-dashboard',
-    styleUrl: 'app-dashboard.css'
+    styleUrl: 'app-dashboard.scss'
 })
 export class AppDashboard {
 
@@ -10,9 +10,15 @@ export class AppDashboard {
         {
             id: 1,
             matchedSkills: ['english', 'skill2', 'business' ],
-            name: 'link',
+            name: 'Link',
             avatar: 'https://i.kym-cdn.com/entries/icons/original/000/017/517/images_(1).jpg'
-        }
+        },
+        {
+            id: 2,
+            matchedSkills: ['marketing', 'tutoring', 'business' ],
+            name: 'Look-a-like',
+            avatar: 'https://i.kym-cdn.com/entries/icons/original/000/017/517/images_(1).jpg'
+        }        
     ];
 
     renderSkills(skills) {
@@ -33,17 +39,21 @@ export class AppDashboard {
     render() {
         //dashboard
         return (
-            <div class="dashboard">
+            <ion-card class="dashboard">
                 {
                     this.profiles.map( profile => (
                         <ion-list>
-                            <ion-item><img src={profile.avatar} /></ion-item>
-                            <ion-item>{profile.name}</ion-item>
-                            <ion-item>{this.renderSkills(profile.matchedSkills)}</ion-item>
+                            <ion-item>
+                                <img slot="start" src={profile.avatar} />
+                            <div>   
+                                <h2>{profile.name}</h2>
+                                {this.renderSkills(profile.matchedSkills)}
+                            </div> 
+                            </ion-item>
                         </ion-list>
                     ))                        
                 }
-            </div>
+            </ion-card>
         );
     }
 }
